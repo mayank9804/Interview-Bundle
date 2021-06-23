8827d5fb63a2ec0aa055fb220ae312819b881ac7
Mayank <mayank04111998@gmail.com>
2021-06-24 01:34:55 +0530

    NextFirstSmallerElements

--- CPLibrary/src/com/codr/framework/Stack/StackUtils.java
+++ CPLibrary/src/com/codr/framework/Stack/StackUtils.java
@@ -48,4 +48,38 @@ public class StackUtils {
 
         return Arrays.asList(result);
     }
+
+    /**
+     * The next greater element of some element x in an array is the first smaller element that is to the right of x in the same array.
+     * @param input
+     * @return
+     */
+    public static List<Integer> computeNextSmallerElements(final List<Integer> input) {
+        if (input.isEmpty()) return input;
+        AtomicInteger integer = new AtomicInteger(0);
+
+        List<Pair<Integer, Integer>> indexedInput = new ArrayList<>();
+        for (Integer e : input) {
+            indexedInput.add(new Pair<>(e, integer.getAndIncrement()));
+        }
+
+        Integer[] result = new Integer[input.size()];
+        Stack<Pair<Integer, Integer>> stack = new Stack<>();
+        stack.add(indexedInput.get(0)); //1,0
+
+        for (int i = 1; i < indexedInput.size(); i++) {
+            while (!stack.empty() && indexedInput.get(i).getKey() <= stack.peek().getKey()) {
+                result[stack.peek().getValue()] = indexedInput.get(i).getKey();
+                stack.pop();
+            }
+            stack.push(indexedInput.get(i));
+        }
+
+        while (!stack.empty()) {
+            result[stack.peek().getValue()] = -1;
+            stack.pop();
+        }
+
+        return Arrays.asList(result);
+    }
 }
